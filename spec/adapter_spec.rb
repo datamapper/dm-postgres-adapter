85ffc2f1af3f5418e0542564ee389e54c341b290
snusnu <gamsnjaga@gmail.com>
2010-04-09 02:27:14 +0200

    No more need to require an explicit automigration extension
    See http://is.gd/bkM8K

--- spec/adapter_spec.rb
+++ spec/adapter_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
-require 'dm-migrations/adapters/dm-postgres-adapter'
-require 'dm-postgres-adapter'
+require 'dm-migrations'
 
 require 'dm-core/spec/adapter_shared_spec'
 require 'dm-do-adapter/spec/shared_spec'
 
-describe DataMapper::Adapters::PostgresAdapter do
+describe 'DataMapper::Adapters::PostgresAdapter' do
 
   before :all do
     @adapter    = DataMapper.setup(:default, 'postgres://localhost/dm_core_test')
